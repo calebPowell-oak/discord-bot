1311a914a7edf3cc925f9028956a2f5f449b0c89
Fused3195 <fusedrenegade@gmail.com>
2020-10-18 00:44:26 -0400

    Updates for Caleb

---
+++ src/index.js
@@ -0,0 +1,50 @@
+const Discord = require('discord.js')
+const { prefix, token } = require('../config.json')
+const client = new Discord.Client()
+const schedule = require('node-schedule')
+const https = require('https')
+const { StringDecoder } = require('string_decoder')
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on('message', msg => {
+  if (msg.content.startsWith(`${prefix}Luke`)) {
+    msg.channel.send("Hi my name is Lucas Liney AND I STOLE FROM  MY MOM'S PURSE")
+  }
+  if (msg.content.startsWith(`${prefix}Pavone`)) {
+    msg.channel.send("https://cdn.discordapp.com/attachments/97076140069359616/766420029226352680/image0.png")
+  }
+  if (msg.content.startsWith(`${prefix}Cows`)) {
+    msg.channel.send("https://i.ytimg.com/vi/TInoP_KtAzw/maxresdefault.jpg")
+  }
+  if (msg.content.startsWith(`${prefix}Frog`)) {
+    msg.channel.send("https://img.fireden.net/ic/image/1553/75/1553758320439.png")
+  }
+  if (msg.content.startsWith(`${prefix}mtgFrog`)) {
+    msg.channel.send("https://mtgcardsmith.com/view/complete/full/2019/7/9/1562676542892670.png")
+  }
+})
+
+client.login(token)
+
+
+var rule = new schedule.RecurrenceRule();
+rule.hour = 19;
+rule.minute = 00;
+ 
+var j = schedule.scheduleJob(rule, function() {
+    client.channels.fetch('363436676699127810').then((channel) => {
+        channel.send("https://i.ytimg.com/vi/TInoP_KtAzw/maxresdefault.jpg")
+    })
+})
+
+// let threads
+// https.get('https://a.4cdn.org/po/threads.json', (res) => {
+//     res.on('data', d => {
+//         threads = JSON.parse(d.toString())
+//         console.log(threads)
+//       })
+// })
+
